b8849c04a3c75dc2ad684e2dc0f641cec9d5d4bd
Jonas Wernimont <werjo179@school.lu>
2024-03-21 13:35:19 +0100

    first commit.

---
+++ sens/guard.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import time
+import RPi.GPIO as GPIO
+
+# Set up GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Define sensor pin
+SENSOR_PIN = 18
+
+# Set up sensor pin
+GPIO.setup(SENSOR_PIN, GPIO.IN)
+
+# Function to get the image file name with timestamp
+def get_next_image_filename():
+    current_time = time.strftime("%d-%m-%Y-%H:%M:%S")
+    filename = f"/home/pi/sens/img/{current_time}.jpg"
+    return filename
+
+# Read voltage of GPIO18 and capture image if voltage is 1
+try:
+    while True:
+        voltage = GPIO.input(SENSOR_PIN)
+        
+        if voltage == 1:
+            next_image_filename = get_next_image_filename()
+            os.system(f"libcamera-jpeg -n -o {next_image_filename} > /dev/null 2>&1")
+
+        time.sleep(0.5)  # Sleep for 0.5 seconds (half a second)
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    GPIO.cleanup()
