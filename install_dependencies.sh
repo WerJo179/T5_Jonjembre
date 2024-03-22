d8328f8c776e0daf276871b5fe31a67b3374ca1c
WerJo179 <164336972+WerJo179@users.noreply.github.com>
2024-03-22 15:04:50 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@
 # Update package lists and upgrade installed packages
 sudo apt update && sudo apt upgrade -y
 
-# Install libcamera-utils, Python 3, Python 3 development packages, pip, and venv
+# Install Python 3, Python 3 development packages, pip, and venv
 sudo apt install python3 python3-dev python3-pip python3-venv git -y
 
 # Install necessary Python packages using pip3
