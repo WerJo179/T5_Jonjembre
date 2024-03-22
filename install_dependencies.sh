8f229d0224e5bfb0582a0d85b59ff01f01caa071
WerJo179 <164336972+WerJo179@users.noreply.github.com>
2024-03-22 15:03:16 +0100

    Added install_dependencies.sh file

---
+++ install_dependencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package lists and upgrade installed packages
+sudo apt update && sudo apt upgrade -y
+
+# Install libcamera-utils, Python 3, Python 3 development packages, pip, and venv
+sudo apt install python3 python3-dev python3-pip python3-venv git -y
+
+# Install necessary Python packages using pip3
+sudo pip3 install adafruit-circuitpython-dht RPi.GPIO spidev mfrc522 boto3 -y
